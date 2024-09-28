a5630b6ac057668aa75e1f989789b84cde155e50
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2024-09-27 22:24:15 -0300

    feat(register): provides the possibility of registering new vehicles

--- src/components/vehicleLiveMap.tsx
+++ src/components/vehicleLiveMap.tsx
@@ -1,16 +1,38 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
+import VehicleForm from "./vehicleForm";
 import { Vehicle } from "@/types/types";
 import { BASE_URL } from "@/utils/localhost";
 
 export default function VehicleMap() {
   const [vehicles, setVehicles] = useState<Vehicle[]>([]);
 
+  const handleAddVehicle = async (vehicle: Vehicle) => {
+    try {
+      const response = await fetch(`${BASE_URL}/vehicles`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(vehicle),
+      });
+
+      if (response.ok) {
+        const newVehicle = await response.json();
+        setVehicles((prevVehicles) => [...prevVehicles, newVehicle]);
+      } else {
+        console.error("Erro ao cadastrar veículo:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  };
+
   useEffect(() => {
     const fetchVehicles = async () => {
       try {
-        const response = await fetch(`http://${BASE_URL}/vehicles`);
+        const response = await fetch(`${BASE_URL}/vehicles`);
         const data = await response.json();
         setVehicles(data);
       } catch (error) {
@@ -22,20 +44,23 @@ export default function VehicleMap() {
   }, []);
 
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: "100vh", width: "100vw" }}
-    >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {vehicles.map((vehicle) => (
-        <Marker key={vehicle.id} position={[vehicle.lat, vehicle.lng]}>
-          <Popup>
-            {vehicle.type} - {vehicle.placa} <br /> Velocidade: {vehicle.speed}{" "}
-            km/h
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div>
+      <VehicleForm onAddVehicle={handleAddVehicle} />
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        style={{ height: "80vh", width: "100vw" }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {vehicles.map((vehicle) => (
+          <Marker key={vehicle.id} position={[vehicle.lat, vehicle.lng]}>
+            <Popup>
+              {vehicle.type} - {vehicle.placa} <br /> Velocidade:{" "}
+              {vehicle.speed} km/h
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
